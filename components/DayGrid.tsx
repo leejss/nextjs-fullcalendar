227613cb80bd40cd6ad323db0f71f290758f5c59
JakeLee <jake.lee@jalsalda.com>
2021-12-20 18:45:16 +0900

    comt

--- components/DayGrid.tsx
+++ components/DayGrid.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGrid from "@fullcalendar/daygrid";
-import { DayCellContentArg, DayHeaderContentArg } from "@fullcalendar/common";
+import {
+  DayCellContentArg,
+  DayHeaderContentArg,
+  EventContentArg,
+  EventSourceInput,
+} from "@fullcalendar/common";
 import { format, add } from "date-fns";
 import { ko } from "date-fns/locale";
 import cx from "classnames";
@@ -12,10 +17,35 @@ interface DayGridProps {
   weeks?: number;
 }
 
+interface EventViewerProps {
+  eventInfo: EventContentArg;
+}
+
+// Need to be synchronized
+const sampleEvent: EventSourceInput = [
+  {
+    title: "Football",
+    date: "2021-12-22",
+  },
+];
+
 // Global Style
 const DayGrid: React.FC<DayGridProps> = ({ height, weeks = 1 }) => {
   const [mounted, setMounted] = React.useState(false);
   const [currentDate, setCurrentDate] = React.useState(new Date());
+  const [clickedEvent, setClickedEvent] =
+    React.useState<EventContentArg | null>(null);
+
+  const renderEventContent = React.useCallback((args: EventContentArg) => {
+    return (
+      <div
+        className="event"
+        onClick={() => {
+          setClickedEvent(args);
+        }}
+      />
+    );
+  }, []);
 
   React.useEffect(() => {
     setMounted(true);
@@ -32,14 +62,23 @@ const DayGrid: React.FC<DayGridProps> = ({ height, weeks = 1 }) => {
           headerToolbar={false}
           dayHeaderContent={renderDayHeaderContent}
           dayCellContent={renderDayCellContent}
+          eventContent={renderEventContent}
           contentHeight={height ?? 120}
+          eventBackgroundColor="transparent"
+          eventTextColor="#000"
           dayCount={7}
+          events={sampleEvent}
         />
       </div>
     ));
   };
   return (
-    <div className="calendarContainer">{mounted ? renderCalendar() : null}</div>
+    <div>
+      <div className="calendarContainer">
+        {mounted ? renderCalendar() : null}
+      </div>
+      {clickedEvent ? <EventViewer eventInfo={clickedEvent} /> : null}
+    </div>
   );
 };
 
@@ -58,3 +97,17 @@ function renderDayHeaderContent(args: DayHeaderContentArg) {
   const week = isToday ? "오늘" : format(date, "E", { locale: ko });
   return <div className={classNames}>{week}</div>;
 }
+
+// function renderEventContent(args: EventContentArg) {
+//   return <div className="event" />;
+// }
+
+const EventViewer: React.FC<EventViewerProps> = ({ eventInfo }) => {
+  return <div>{eventInfo.event.title}</div>;
+};
+
+/**
+ * Date click 시 일어나는 로직,
+ * 날짜 클릭 시,
+ *
+ */
